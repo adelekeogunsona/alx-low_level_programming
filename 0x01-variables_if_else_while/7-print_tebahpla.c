ca76198f140f6b5ad08cb7318f63c186d1b7ad99
Adeleke <ogunsonaadedotun@gmail.com>
2022-09-09 07:50:11 -0700

    Commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: always 0
+*/
+int main(void)
+{
+	char alphabets;
+
+	for (alphabets = 'z'; alphabets >= 'a'; alphabet--)
+		putchar(alphabets);
+
+	putchat('\n');
+
+	return (0);
+}
