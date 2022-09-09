6b1220ab22c61a4eecc871032159bdc3f342e75c
Adeleke <ogunsonaadedotun@gmail.com>
2022-09-09 08:49:10 -0700

    Commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: always 0
+*/
+int main(void)
+{
+	int digits1;
+	int number1;
+	int digits2;
+	int number2;
+
+	for (digits1 = 0; digits1 < 10; digits1++)
+	{
+		number1 = ((digits1 % 10) + '0');
+
+		for (digits2 = 0; digits2 < 10; digits2++)
+		{
+			number2 = ((digits2 % 10) + '0');
+
+			if (number1 != number2)
+			{
+				putchar(number1);
+				putchar(number2);
+
+				if (number1 != 9 && number2 != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
