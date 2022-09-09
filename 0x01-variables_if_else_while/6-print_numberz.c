369d12e9f4ee3c7d73fcb8fc2338a4197bdb602e
Adeleke <ogunsonaadedotun@gmail.com>
2022-09-09 07:40:56 -0700

    Commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: always 0
+*/
+int main(void)
+{
+	int digits;
+
+	for (digits = 0; digits < 10; digits++)
+		putchar(digits);
+
+	putchar('\n');
+
+	return (0);
+}
