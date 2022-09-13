be5137bc88ec4ac00f84b7a255dc681d56915a26
Adeleke <ogunsonaadedotun@gmail.com>
2022-09-13 06:57:37 -0700

    Commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _abs - Function that computes the absoulte value of an integer
+* @n: Number to be checked
+*
+* Return: Always 0
+*/
+int _abs(int n)
+{
+	int absolute;
+
+	absolute = (n * n) / n;
+
+	return (n);
+}
