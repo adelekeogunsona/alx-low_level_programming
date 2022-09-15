1a0ec8c6cf984a88b19b97dcfa4d4c628d97b6d2
Adeleke <ogunsonaadedotun@gmail.com>
2022-09-15 14:22:05 +0000

    Fixed errors

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,13 +9,13 @@
 
 void print_square(int size)
 {
-	int x;
+	int a, b;
 
 	if (size > 0)
 	{
-		for (x = 0; x < size; x++)
+		for (a = 0; a < size; a++)
 		{
-			for (x = 0; x < size; x++)
+			for (b = 0; b < size; b++)
 			{
 				_putchar(35);
 			}
