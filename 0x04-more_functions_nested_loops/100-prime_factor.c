4a9fc57f4a079f1fd1fbbbbf3f37e4c5afd50cff
Adeleke <ogunsonaadedotun@gmail.com>
2022-09-15 15:40:08 +0000

    Prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+* main - Entry Point
+*
+* Return: zero
+*/
+
+int main(void)
+{
+	long long n = 612852475143;
+	int i = 2, largest = 2;
+
+	while (n != 1)
+	{
+		while ((n % i) == 0)
+		{
+			n = n / i;
+			largest = i;
+		}
+
+		i++;
+	}
+
+	printf("%d\n", largest);
+
+	return (0);
+}
