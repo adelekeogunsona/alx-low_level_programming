3fa17783eddc6d176d9eaf18c8947267be6260b1
Adeleke <ogunsonaadedotun@gmail.com>
2022-09-15 13:35:01 +0000

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_line - Draws a straight line
+* @n: Length of line
+*
+* Return: zero
+*/
+
+void print_line(int n)
+{
+	int x;
+
+	if (n > 0)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			_putchar(95);
+		}
+
+		_putchar("\n");
+	}
+	else
+	{
+		_putchar("\n");
+	}
+}
