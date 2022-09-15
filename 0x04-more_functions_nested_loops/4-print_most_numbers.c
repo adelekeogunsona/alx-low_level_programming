a7dc0b6d5b1af0cfd1af17668eb4e70849795cbf
Adeleke <ogunsonaadedotun@gmail.com>
2022-09-15 12:53:28 +0000

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_numbers - Print the number  0-9
+*
+* Return: The  number 0-9
+*/
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num != 2 && num != 4)
+			_putchar(num + '0');
+	}
+
+	_putchar('\n');
+}
+
