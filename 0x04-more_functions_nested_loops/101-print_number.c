e16757402ab78e9c7ee5b697b757dea7ba6087e0
Adeleke <ogunsonaadedotun@gmail.com>
2022-09-15 15:56:17 +0000

    Fixed function to print integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,13 +1,29 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 * print_number - Prints an integer
-* @n: Integer to be printed
+* @n: The integer to be printed
 *
 * Return: void
 */
 
 void print_number(int n)
 {
-	_putchar(n + '0');
+	unsigned int a = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		a = n;
+		_putchar('-');
+	}
+
+	a /= 10;
+
+	if (a != 0)
+		print_number(a);
+
+	_putchar((unsigned int) n % 10 + '0');
+
 }
