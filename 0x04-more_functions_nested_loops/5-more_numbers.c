3cdf4979a12a19bfae3584744bf04f548ac6f9ed
Adeleke <ogunsonaadedotun@gmail.com>
2022-09-15 13:14:05 +0000

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,17 @@
 
 void more_numbers(void)
 {
-	int num;
-	int i;
+	int num, i;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+				_putchar((num / 10) + '0');
+
 			_putchar(num + '0');
+		}
 
 		_putchar("\n");
 	}
