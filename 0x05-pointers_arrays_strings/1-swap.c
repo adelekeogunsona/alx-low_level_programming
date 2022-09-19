ced6607cf1b69e68504f88671b0b13071a231f96
Adeleke <ogunsonaadedotun@gmail.com>
2022-09-19 12:31:14 +0000

    Added function to swap arguments

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* swap_int - Function that swaps the values of two integers
+* @a: First value
+* @b: Second value
+*/
+
+void swap_int(int *a, int *b)
+{
+	first = *a;
+	second = *b;
+
+	*a = second;
+	*b = first;
+}
