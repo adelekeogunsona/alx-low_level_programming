da171454cc184841d6aa84ee14abe081a70a0a49
Adeleke <ogunsonaadedotun@gmail.com>
2022-09-19 13:02:34 +0000

    Added function that prints to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _puts - Function that prints to stdout
+* @str: String to be printed
+*
+* Return: void
+*/
+
+void _puts(char *str)
+{
+	printf("%s\n", *str);
+}
