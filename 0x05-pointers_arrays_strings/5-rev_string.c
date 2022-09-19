b7e79c1ff6269cc4d77f624a5b75fdd28bbd94c9
Adeleke <ogunsonaadedotun@gmail.com>
2022-09-19 14:22:55 +0000

    Added function to reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* rev_string - Prints a string in reverse
+* @s: String to be printed
+*
+* Return: void
+*/
+
+void rev_string(char *s)
+{
+	int count = -1;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+
+	char reverse[];
+	int i;
+
+	for (; count >= 0; count--)
+	{
+		i = 0;
+		char reverse[i] = s[count];
+	}
+
+	*s = reverse;
+}
