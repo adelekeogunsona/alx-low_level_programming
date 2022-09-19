c95ae7b7f0d2c9ca010873fa90bae776ecd83ef7
Adeleke <ogunsonaadedotun@gmail.com>
2022-09-19 15:48:13 +0000

    Added function that prints nth elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_array - Prints nth number of an array
+* @a: array
+* @n: number of elements to print
+*
+* Return: void
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+	int times;
+
+	times = n - 1;
+
+	for (i = 0; i <= times; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i < times)
+			printf(", ");
+	}
+}
