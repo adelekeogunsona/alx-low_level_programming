1e2fdaddecd5695f4f632e09baa79035343ebc81
Adeleke <ogunsonaadedotun@gmail.com>
2022-09-22 16:35:52 +0000

    Added function to encode string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,26 +9,17 @@
 
 char *leet(char *s)
 {
-	int i, j;
-	char sub[] = "AaEeOoTtLl";
+	int i, j, k, l;
+	char sub[] = "AEOTL";
 	char rep[] = {52, 51, 48, 55, 49};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; sub[j] != '\0'; j++)
 		{
-			if (s[i] == sub[j])
+			if (s[i] == sub[j] || s[i] == (sub[j] + 32))
 			{
-				if (j == 0 || j == 1)
-					s[i] = rep[0];
-				else if (j == 2 || j == 3)
-					s[i] = rep[1];
-				else if (j == 4 || j == 5)
-					s[i] = rep[2];
-				else if (j == 6 || j == 7)
-					s[i] = rep[3];
-				else
-					s[i] = rep[4];
+				s[i] = rep[j];
 			}
 		}
 	}
