5e541704ea065d39b765c1ff89adf2404e4cd2e6
Adeleke <ogunsonaadedotun@gmail.com>
2022-09-22 14:54:24 +0000

    Added function that capitalizes words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* cap_string - Capitalizes all words of a string
+* @c: String
+*
+* Return: c
+*/
+
+char *cap_string(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] == 32 || c[i]  == 9 || c[i] == 10 || c[i] == 44 || c[i] == 59 || c[i] == 46 || c[i] == 33 || c[i] == 63 || c[i] == 34 || c[i] == 40 || c[i] == 41 || c[i] == 123 || c[i] == 124)
+		{
+			if (c[i + 1] >= 97 && c[i + 1] <= 122)
+			{
+				c[i + 1] -= 32;
+			}
+		}
+	}
+
+	return (c);
+}
