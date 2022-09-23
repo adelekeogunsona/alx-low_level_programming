50ea51479e78906fab859f2d7a1c3baf15620191
Adeleke <ogunsonaadeleke@yahoo.com>
2022-09-23 16:21:35 +0100

    Added function that adds two numbers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,6 +11,7 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *s);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 
 #endif /* MAIN_H */
