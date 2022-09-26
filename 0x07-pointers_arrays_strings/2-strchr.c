e85944877e5d3f62867e085b69726db61a3c6026
Adeleke <ogunsonaadedotun@gmail.com>
2022-09-26 13:25:37 +0100

    Added function that locates a character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,13 +13,16 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s)
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*s != c)
-			s++;
-		else
-			return (s);
+		if (s[i] == c)
+		{
+			return (s[i]);
+		}
 	}
+
 	if (c == '\0')
 		return (s);
 
