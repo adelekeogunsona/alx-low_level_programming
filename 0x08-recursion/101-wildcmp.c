ecd51189ad36586c030628216866d62a3303644a
Adeleke <ogunsonaadeleke@yahoo.com>
2022-09-29 16:44:52 +0100

    Added function that compares 2 strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,8 +10,10 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == *s2)
-		return (1);
-	else
+	if (*s1 == '\0')
 		return (0);
+	if (*s1 == *s2)
+		return (wildcmp((s1 + 1), (s2 + 1)));
+
+	return (0);
 }
