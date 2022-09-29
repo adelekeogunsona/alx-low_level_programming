2ed25dbfda7622cae725ddbd328b52f6eb9f4c80
Adeleke <ogunsonaadeleke@yahoo.com>
2022-09-29 16:27:21 +0100

    Added function that compares 2 strings

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* wildcmp - Compares 2 strings
+* @s1: String one
+* @s2: String two
+*
+* Return: 1 if strings are identical, 0 otherwise
+*/
+
+int wildcmp(char *s1, char *s2)
+{
+
+}
