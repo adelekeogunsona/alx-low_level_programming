722933070f81ed9393891ceb33500bc45abc2bed
Adeleke <ogunsonaadeleke@yahoo.com>
2022-09-30 08:01:18 +0100

    Added function to print program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * 
- * Return: always zero
+* main - Entry point
+* @argc: Argument count
+* @argv: Argument vector
+*
+* Return: always zero
 */
 
-int main (int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	if (argc > 0)
 		printf("%s\n", argv[0]);
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Return: always zero
+*/
+
+int main (int argc, char* argv[])
+{
+	return (0);
+}
