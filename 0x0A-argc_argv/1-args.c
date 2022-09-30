ae3c587d24171d54f6ad8edab483e06813876986
Adeleke <ogunsonaadeleke@yahoo.com>
2022-09-30 08:07:58 +0100

    Added function that prints argument count

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * 
- * Return: always zero
+* main - Entry point
+* @argc: Argument count
+* @argv: Argument vector
+*
+* Return: always zero
 */
 
-int main (int argc, char* argv[])
+int main (int argc, __attribute__ ((unused)) char *argv[])
 {
+	printf("%d\n", argc - 1);
+
 	return (0);
 }
