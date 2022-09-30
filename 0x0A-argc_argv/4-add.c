064a33259ccb31ba4f63d2a3975779c2db3b15c8
Adeleke <ogunsonaadeleke@yahoo.com>
2022-09-30 09:36:11 +0100

    Added program that adds positive integers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,14 +3,37 @@
 #include <ctype.h>
 
 /**
+* check_number - Checks if string is a number
+* @s: String
+*
+* Return: 0 if true, 1 otherwise
+*/
+
+int check_number(char *s)
+{
+	int i, check = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (!isdigit(s[i]))
+		{
+			check = 1;
+			break;
+		}
+		else
+			check = 0;
+	}
+
+	return (check);
+}
+
+/**
 * main - Entry point
 * @argc: Argument count
 * @argv: Argument vector
 *
 * Return: Always zero
 */
-
-int check_number(char *s);
 
 int main(int argc, char *argv[])
 {
@@ -36,22 +59,4 @@ int main(int argc, char *argv[])
 	}
 
 	return (0);
-}
-
-int check_number (char *s)
-{
-	int i, check = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (!isdigit(s[i]))
-		{
-			check = 1;
-			break;
-		}
-		else
-			check = 0;
-	}
-
-	return (check);
 }
