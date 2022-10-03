6794d81fb1875dc4be417ffa6c5ff918c565e87e
Adeleke <ogunsonaadeleke@yahoo.com>
2022-10-03 11:52:27 +0100

    Added function to concatenate strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,6 +15,11 @@ char *str_concat(char *s1, char *s2)
 	int i, j = 0;
 	char *join;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[len1] != '\0')
 		len1++;
 	while (s2[len2] != '\0')
