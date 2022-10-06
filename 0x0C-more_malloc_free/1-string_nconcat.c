8e71f57f85dab25cbcde5b7390b868991e42ac3d
Adeleke <ogunsonaadeleke@yahoo.com>
2022-10-06 14:06:39 +0100

    Added function to concatenate string dynamically

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - Concatenates two strings
+ * @s1: string one
+ * @s2: string two
+ * @n: number of bytes to use from s2
+ *
+ * Return: pointer to concatenated string or NULL on error
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int a = 0, b = 0, i, j, k, l;
+	char *concat;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[a] != '\0')
+		a++;
+	while (s2[b] != '\0')
+		b++;
+
+	concat = malloc(a + b + 1);
+
+	if (n >= b)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			*(concat + i) = s1[i];
+		for (j = 0; j < b; j++)
+		{
+			*(concat + i) = s2[j];
+			i++;
+		}
+	}
+	else
+	{
+		for (k = 0; s1[k] != '\0'; k++)
+			*(concat + k) = s1[k];
+		for (l = 0; l < n; l++)
+		{
+			*(concat + k) = s2[l];
+			k++;
+		}
+	}
+
+	return (concat);
+}
