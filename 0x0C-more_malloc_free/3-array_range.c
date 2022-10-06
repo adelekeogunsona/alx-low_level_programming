f65f4df7926831855939b1eaaea74717662a4746
Adeleke <ogunsonaadeleke@yahoo.com>
2022-10-06 15:11:54 +0100

    Added function to dynamically create an array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,14 +13,18 @@ int *array_range(int min, int max)
 {
 	int i = 0;
 	int j = 0;
-	int *p;
+	int *arr;
 
 	if (min > max)
 		return (NULL);
-	p = malloc((max - min + 1) * sizeof(int));
-	if (p == NULL)
+
+	arr = malloc(sizeof(int) * (max - min + 1));
+
+	if (arr == NULL)
 		return (NULL);
+
 	for (i = min; i <= max; i++, j++)
-		p[j] = i;
-	return (p);
+		arr[j] = i;
+
+	return (arr);
 }
