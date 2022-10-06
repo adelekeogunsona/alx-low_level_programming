7fa2b6954de06e2f6c657508cff2eec87a578029
Adeleke <ogunsonaadeleke@yahoo.com>
2022-10-06 14:13:06 +0100

    Added function to concatenate string dynamically

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,10 +25,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2[b] != '\0')
 		b++;
 
-	concat = malloc(a + b + 1);
-
 	if (n >= b)
 	{
+		concat = malloc(sizeof(char) * (a + b) + 1);
 		for (i = 0; s1[i] != '\0'; i++)
 			*(concat + i) = s1[i];
 		for (j = 0; j < b; j++)
@@ -39,6 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
+		concat = malloc(sizeof(char) * (a + n) + 1);
 		for (k = 0; s1[k] != '\0'; k++)
 			*(concat + k) = s1[k];
 		for (l = 0; l < n; l++)
