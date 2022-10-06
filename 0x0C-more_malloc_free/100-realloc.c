bd03bf3125360a7d44de8c73cecabb7bbe8e7ab0
Adeleke <ogunsonaadeleke@yahoo.com>
2022-10-06 16:15:55 +0100

    Added realloc function

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,16 +14,17 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	unsigned int i;
 	char *new;
-	char *temp;
+	char *temp = ptr;
 
 	if (new_size == old_size)
 		return (ptr);
 
 	else if (ptr == NULL)
 	{
-		new = malloc(new_size);
-		if (new == NULL)
+		ptr = malloc(new_size);
+		if (ptr == NULL)
 			return (NULL);
+		return (ptr);
 	}
 
 	else if (new_size == 0 && ptr != NULL)
@@ -38,12 +39,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		if (new == NULL)
 			return (NULL);
 
-		temp = ptr;
 		for (i = 0; i < new_size; i++)
 		{
 			new[i] = temp[i];
 		}
 		free(ptr);
 	}
+
 	return (new);
 }
