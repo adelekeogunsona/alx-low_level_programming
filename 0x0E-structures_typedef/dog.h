429d4a32e31e4f5f954ec3da626a783ffb626454
Adeleke <ogunsonaadeleke@yahoo.com>
2022-10-10 16:41:18 +0100

    Added dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - creating a structure for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
