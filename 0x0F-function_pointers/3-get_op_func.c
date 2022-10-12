15684929308bc7308277f03f31714a00f511e32d
Adeleke <ogunsonaadeleke@yahoo.com>
2022-10-12 15:35:24 +0100

    Built calc program

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -10,8 +10,6 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-	int i = 0;
-
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -20,12 +18,10 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
+	int i = 0;
 
-	while (s != ops[i].op)
-	{
-		if (*s == *(ops[i].op))
-			return (ops[i].f);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
-	return (0);
+
+	return (ops[i].f);
 }
