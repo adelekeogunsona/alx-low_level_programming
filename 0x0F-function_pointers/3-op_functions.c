7b28ed037308f73a96677abeb34c6aafb9a2d4b8
Adeleke <ogunsonaadeleke@yahoo.com>
2022-10-12 15:11:14 +0100

    Built calc program

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * op_add - Adds 2 integers
+ * @a: number one
+ * @b: number two
+ *
+ * Return: result
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtract 2 integers
+ * @a: number one
+ * @b: number two
+ *
+ * Return: result
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplies 2 integers
+ * @a: number one
+ * @b: number two
+ *
+ * Return: result
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides 2 integers
+ * @a: number one
+ * @b: number two
+ *
+ * Return: result
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Modulus
+ * @a: number one
+ * @b: number two
+ *
+ * Return: result
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
