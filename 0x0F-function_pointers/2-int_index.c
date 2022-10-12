e784dea913c94df0bdefa5b3b879f1e027138965
Adeleke <ogunsonaadeleke@yahoo.com>
2022-10-12 14:24:43 +0100

    Added function that returns index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index - Returns the index of first occurence of 0
+ * @array: array to be indexed
+ * @size: size of the array
+ * @cmp: function pointer
+ *
+ * Return: the index that matches the search
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, check, match = -1;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		check = cmp(array[i]);
+		if (check != 0)
+		{
+			match = i;
+			break;
+		}
+	}
+
+	return (match);
+}
