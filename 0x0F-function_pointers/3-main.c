df8f51ecca531c3869adf4c0bb52660b29517254
Adeleke <ogunsonaadeleke@yahoo.com>
2022-10-12 15:46:38 +0100

    Built calc program

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -21,8 +21,8 @@ int main(int argc, char *argv[])
 	}
 
 	num1 = atoi(argv[1]);
-	op = argv[2];
 	num2 = atoi(argv[3]);
+	op = argv[2];
 
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
@@ -30,8 +30,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if ((*op == '/' && num2 == 0) ||
-	    (*op == '%' && num2 == 0))
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
